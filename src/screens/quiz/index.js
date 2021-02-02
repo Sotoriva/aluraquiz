c4e289f6f585e5f2857f2edb1a7d8687f640a813
Sotoriva <joaosotoriva@hotmail.com>
2021-02-02 01:03:58 -0300

    remove comment in resultWidget propType

--- src/screens/quiz/index.js
+++ src/screens/quiz/index.js
@@ -65,9 +65,9 @@ function ResultWidget({ results }) {
   );
 }
 
-// ResultWidget.propTypes = {
-//   results: PropTypes.arrayOf(PropTypes.array).isRequired,
-// };
+ResultWidget.propTypes = {
+  results: PropTypes.arrayOf(PropTypes.array).isRequired,
+};
 
 function LoadingWidget() {
   return (
